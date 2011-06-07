6dc3af7bb035bd67fc28058b518fd05c3b733d6f
philippm <philipp@incrediblebee.com>
2011-06-07 13:17:32 +0200

    Don't install iESiTunesScroller when on 10.7+

--- ESScrollView.m
+++ ESScrollView.m
@@ -13,10 +13,19 @@
 
 - (void)awakeFromNib;
 {
+	// Revert to the default iPad-like scrollbars on Lion and later
+	SInt32 majorVersion, minorVersion;
+	Gestalt(gestaltSystemVersionMajor, &majorVersion);
+	Gestalt(gestaltSystemVersionMinor, &minorVersion);
+	if (majorVersion >= 10 && minorVersion >= 7) {
+//		NSLog(@"On Lion or later, reverting to default scroll bar");   // DEBUG
+		return;
+	}
+
 	NSRect horizontalScrollerFrame = [[self horizontalScroller] frame];
 	NSRect verticalScrollerFrame = [[self verticalScroller] frame];
 	NSString *scrollBarVariant = [[[NSUserDefaults standardUserDefaults] persistentDomainForName:NSGlobalDomain] valueForKey:@"AppleScrollBarVariant"];
-	
+
 	if (![scrollBarVariant isEqualToString:@"DoubleBoth"]) {
 		[self setVerticalScroller:[[[ESiTunesScroller alloc] initWithFrame:verticalScrollerFrame] autorelease]];
 		[self setHorizontalScroller:[[[ESiTunesScroller alloc] initWithFrame:horizontalScrollerFrame] autorelease]];
